ba383aff281e79b68dc945dd5463e53c665fd79f
aidp4 <aidp4@TEST>
2022-05-29 16:07:45 +1000

    Add project files.

---
+++ Auto Accept/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+using IronOcr;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+
+while (true)
+{
+    void Main()
+    {
+        var Ocr = new IronTesseract();
+        Bitmap memoryImage;
+        memoryImage = new Bitmap(220, 90);
+        Size s = new Size(memoryImage.Width, memoryImage.Height);
+
+        Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        const int MOUSEEVENT_LEFTDOWN = 0x02;
+        const int MOUSEEVENT_LEFTUP = 0x04;
+
+        memoryGraphics.CopyFromScreen(850, 403, 0, 0, s);
+
+        //That's it! Save the image in the directory and this will work like charm. 
+        var filePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonDesktopDirectory) + @"\image.png";
+        string fileName = string.Format(filePath);
+        memoryImage.Save(fileName);
+
+        using (var Input = new OcrInput(fileName))
+        {
+            //Input.Deskew();  // use if image not straight
+            Input.DeNoise(); // use if image contains digital noise
+            var Result = Ocr.Read(Input);
+            Console.WriteLine(Result.Text);
+
+            if (Result.Text != "ACCEPT") // if not accept wait 4 secs and try again
+            {
+                Console.WriteLine("No game ):");
+                Thread.Sleep(3000);
+                Main();
+            }
+            else if (Result.Text == "ACCEPT") // move mouse to accept button and click
+            {
+                SetCursorPos(1050, 420);
+                Console.WriteLine("Game found!");
+                Thread.Sleep(1000);
+                mouse_event(MOUSEEVENT_LEFTDOWN, 0, 0, 0, 0);
+                Thread.Sleep(100);
+                mouse_event(MOUSEEVENT_LEFTUP, 0, 0, 0, 0);
+
+            }
+        }
+
+
+
+    }
+    Main();
+    break;
+}
